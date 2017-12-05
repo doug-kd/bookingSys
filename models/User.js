f0c8be1e5e27770c6ed13dfae9ce0aa92fea0868
Becca Lee <dougward161@gmail.com>
2017-12-05 00:04:20 +0000

    OAuth ok, not writing to database though

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+const { Schema } = mongoose; // restructured from above
+// the above says the mongoose object has a property called Schema. Take it and assign it to a new Var called Schema
+const userSchema = new Schema({
+  googleId: String, // we have a string for the googleID
+  name: String,
+  email: String
+});
+
+mongoose.model('users', userSchema); //I'm loading the schema into mongoose that I'm creating a new collection called users
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport'); // requiring passport
 const GoogleStrategy = require('passport-google-oauth20').Strategy; // only requiring the strategy from the oauth
+const mongoose = require('mongoose');
 const keys = require('../config/keys'); // importing the id and secrect so they are safe in the keys which is in the gitignore as well
+
+const User = mongoose.model('users'); // pulling the schema out on mongoose, can do it with just a single argument one argument is fetching something two arguments means we're loading something into it
 
 passport.use
     (new GoogleStrategy(//telling passport the 'use' (a function) a new GoogleStrategy
@@ -10,9 +13,11 @@ passport.use
   callbackURL: '/auth/google/callback' // the route the user is sent after permission is granted
 },
 (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken); // logging it out but doesn't do anything currently
-    console.log('refresh token', refreshToken); // logging out the refresh token
-    console.log('profile:', profile); // logging out the profile
+    new User({googleId: profile.id}).save //I'm creating a new model instance of a user, the .save takes a model Instance takes it to Mongo world and saves it
     }
   )
 );
+
+// console.log('access token', accessToken); // logging it out but doesn't do anything currently
+// console.log('refresh token', refreshToken); // logging out the refresh token
+// console.log('profile:', profile); // logging out the profile
