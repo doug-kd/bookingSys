efaa68c335ebbdf9645da65e790d90e01640e3d0
doug-kd <31511553+doug-kd@users.noreply.github.com>
2017-12-05 16:34:15 +0000

    Auth working with cookies and writing to Mlabs!

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session'); // makes express care about cookies
+const passport = require('passport');
 const keys = require('./config/keys');
 require ('./models/User');
 require ('./services/passport');
 
-
 mongoose.connect(keys.mongoURI); // connecting to MLabs but keeping our key secures in the config file which is added to the .gitignore file
 
 const app = express();
+
+app.use(
+  cookieSession({ //configuration object
+    maxAge: 30 * 24 * 60 * 60 * 1000, // cookie lasts for 30 days - age has to entered in miliseconds
+    keys: [keys.cookieKey] // brings in the the cookieKey from the Keys folder
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // require returns a function - the second set of parathesis immedity requires thats in them, in this case app
 
--- models/User.js
+++ models/User.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose; // restructured from above
 // the above says the mongoose object has a property called Schema. Take it and assign it to a new Var called Schema
 const userSchema = new Schema({
-  googleId: String, // we have a string for the googleID
-  name: String,
-  email: String
+  googleId: String,
+   // have a string for the googleID
 });
 
 mongoose.model('users', userSchema); //I'm loading the schema into mongoose that I'm creating a new collection called users
